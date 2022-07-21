b90a0ea06fc9b0b941830ccf0ca435a0e975e955
Zuzka <kikucisko@gmail.com>
2022-07-21 14:13:22 +0200

    Add getComputerChoice function, always returning 'paper'

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log('hi');
+
+/*
+Algorithm:
+Computer randomly selects one of 3 choices: rock/paper/scissors
+Ask user for input (rock/paper/scissors)
+Compare results (rock>scissors>paper>rock)
+Output the result
+*/
+
+function getComputerChoice() {
+    return 'paper';
+}
+
+console.log(getComputerChoice());
