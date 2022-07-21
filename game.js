aaa8929accf36f551d0343424cc3226b649bff91
Zuzka <kikucisko@gmail.com>
2022-07-21 14:49:51 +0200

    Working rock/paper/scissors game

--- game.js
+++ game.js
@@ -7,13 +7,57 @@ Compare results (rock>scissors>paper>rock)
 Output the result
 */
 
-function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * 3);
-    if (randomChoice === 0) {
-        return 'rock';
-    } else if (randomChoice === 1) {
-        return 'paper';
-    } else return 'scissors';
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let isInputValid = validatePlayerInput(playerSelection);
+    if (isInputValid) {
+        if (playerSelection === computerSelection) {
+            console.log('Draw! Both chose: ' + computerSelection);
+        } else {
+            let winner = calculateWinner(playerSelection, computerSelection);
+            let winningOption = (winner === 'player') ? playerSelection : computerSelection;
+            let losingOption = (winner === 'player') ? computerSelection : playerSelection;
+            console.log(winner + ' wins! ' + winningOption + ' beats ' + losingOption);
+        }
+    } else {
+        console.log('Player chose unknown option. Quitting :(');
+    }
 }
 
-console.log(`Computer chose: ` + getComputerChoice());
+function validatePlayerInput(input) {
+    return input === 'rock' || input === 'paper' || input === 'scissors';
+}
+
+function calculateWinner(playerSelection, computerSelection) {
+    if ((playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'rock' && computerSelection === 'scissors')) {
+        return 'player';
+    } else return 'computer';
+}
+
+function getComputerChoice() {
+    const randomChoice = Math.floor(Math.random() * 3);
+    let choice = '';
+    if (randomChoice === 0) {
+        choice = 'rock';
+    } else if (randomChoice === 1) {
+        choice = 'paper';
+    } else choice = 'scissors';
+    console.log(`'Computer chose ${choice}'`);
+    return choice;
+}
+
+function getPlayerChoice() {
+    let choice = prompt("Choose your weapon! Rock/Paper/Scissors");
+    console.log(`'Player chose ${choice}'`);
+    return choice;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log('Round ' + +(i + 1));
+        playRound(getPlayerChoice(), getComputerChoice());
+    }
+}
+
